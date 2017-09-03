09dfd0f02f6e924784b5b66bac4ef52bf4d44692
vits71 <vits71@gmail.com>
2017-09-03 20:27:28 +0200

    Implemented edit of the existing food

--- FoodTracker/FoodTracker/MealViewController.swift
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -30,6 +30,14 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Handle the text field’s user input through delegate callbacks.
         nameTextField.delegate = self
         
+        // Set up views if editing existing Meal
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text   = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name
         
         updateSaveButtonSatte()
@@ -79,8 +87,18 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on style of presentation (modal or push), this view controller needs to be dismissed in two different ways
+        if presentingViewController is UINavigationController {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationalController = navigationController {
+            owningNavigationalController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+        
+        
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
         
